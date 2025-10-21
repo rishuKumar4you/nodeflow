e1371815985de563c045845839a597ec693637c9
rishuKumar4you <rishukumarforyou@gmail.com>
2025-10-21 17:49:54 +0530

    Add TRPC integration and update dependencies for improved data fetching

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,23 @@
-import prisma from "@/lib/db";
+import { getQueryClient,trpc } from "@/trpc/server";
+import { Client } from "./client";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
-const Page = async() => {
-  const users = await prisma.user.findMany();
+const Page = async () => {
+  const queryClient = getQueryClient();
 
-
- return( <div>
-    {JSON.stringify(users)}
-  </div>
-    );
+  void queryClient.prefetchQuery(trpc.getUsers.queryOptions())
+  return (
+    <div className="min-h-screen min-w-screen flex items-center
+justify-center">
+      
+      <HydrationBoundary state={dehydrate(queryClient)}>
+        <Suspense fallback={<div>Loading...</div>}>
+          <Client />
+        </Suspense>
+      </HydrationBoundary>
+    </div>
+  );
 };
-
 
 export default Page;
