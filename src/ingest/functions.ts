306367ad147b75aa0e34ba3053338f7e2a6a4c01
rishuKumar4you <rishukumarforyou@gmail.com>
2025-10-22 09:04:27 +0530

    feat: update dependencies and integrate AI SDKs for enhanced functionality

--- src/ingest/functions.ts
+++ src/ingest/functions.ts
@@ -1,21 +1,48 @@
-import { inngest } from "./client";
-import prisma from "@/lib/db";
+import {createGoogleGenerativeAI} from '@ai-sdk/google';
+import {generateText} from 'ai';
+import {createOpenAI} from '@ai-sdk/openai';
+import {inngest} from './client';
+import {createAnthropic} from '@ai-sdk/anthropic';
+
+const google = createGoogleGenerativeAI();
+const openai = createOpenAI();
+const anthropic = createAnthropic();
 
 
-export const helloWorld = inngest.createFunction(
-    { id: "hello/world" },
-    { event: "test/hello-world" },
-    
-    async({ event, step })=> {
-    await step.sleep("wait-a-moment", "5s");
-    
-    
-        await step.run("create-workflow", () => {
-            return prisma.workflow.create({
-                data: {
-                    name: "Workflow-from-inngest",
-                },
+export const execute = inngest.createFunction(
+    {id: 'execute-ai'},
+    {event: 'execute/ai'},
+
+    async ({ event, step }) => {
+        
+        await step.sleep("pretend", "5s")
+        const { steps: geminiSteps } = await step.ai.wrap('gemini-generate-text',
+        generateText, 
+        {
+        system:
+            'You are a helpful assistant that generates text based on user prompts.',
+        prompt: 'what is 2+2?',
+        model: google('gemini-2.5-flash'),
             });
+
+        const {
+          steps: openaiSteps
+        } = await step.ai.wrap('openai-generate-text', generateText, {
+          system:
+              'You are a helpful assistant that generates text based on user prompts.',
+          prompt: 'what is 2+2?',
+          model: openai('gpt-4o'),
         });
-},
+
+        const {
+          steps: anthropicSteps
+        } = await step.ai.wrap('anthropic-generate-text', generateText, {
+          system:
+              'You are a helpful assistant that generates text based on user prompts.',
+          prompt: 'what is 2+2?',
+          model: anthropic('claude-sonnet-4-5'),
+        });
+
+        return { geminiSteps, openaiSteps, anthropicSteps, }
+    },
 );
