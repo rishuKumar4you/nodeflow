e876e586119908fea229e771fe11f58a1bc8f976
rishuKumar4you <rishukumarforyou@gmail.com>
2025-10-23 10:48:59 +0530

    feat: add workflows management with CRUD operations and upgrade modal

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,35 +1,11 @@
-import prisma from '@/lib/db';
-import {google} from '@ai-sdk/google';
-import {generateText} from 'ai';
-import { z } from 'zod';
-import {inngest} from '@/ingest/client';
-
-import {createTRPCRouter, protectedProcedure} from '../init';
+import {workflowsRouter} from '@/features/workflows/server/routers';
+import {createTRPCRouter} from '../init';
 
 export const appRouter = createTRPCRouter({
 
-  testAi: protectedProcedure.mutation(async () => {
-    await inngest.send({
-      name: "execute/ai",
-      
-    });
-
-    return { success: true, message: "Job Queued" }
-  }),
+  workflows: workflowsRouter,
 
 
-  // getWorkflows: protectedProcedure.query(({ctx}) => {
-  //   // console.log({userId: ctx.auth.user.id});
-  //   return prisma.workflow.findMany();
-  // }),
-
-  createWorkflow: protectedProcedure.mutation(() => {
-    return prisma.workflow.create({
-      data: {
-        name: 'Test Workflow',
-      },
-    });
-  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
